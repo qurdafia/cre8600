11003f08c77a231fdef5bc7e8e0cbf85c9cc3fdf
Mark Christian Roma <markchristianroma@Marks-iMac.local>
2022-08-01 12:50:19 +0800

    Adding API calls.

--- pages/contacts/[id].js
+++ pages/contacts/[id].js
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router"
 
-export default function Car() {
+export default function Contacts(data) {
 
-    const router = useRouter()
-    const { id } = router.query
+    const router = useRouter();
+    const { id } = router.query;
+    const contact = data.data;
     
-    return <h1>Hello, {id}!</h1>
+    return <h1>Hello, {contact.name}!</h1>
+
+    
+}
+
+export async function getServerSideProps({params}) {
+    const url = `https://jsonplaceholder.typicode.com/users/${params.id}`
+    const req = await fetch(url);
+    const data = await req.json();
+
+    return {
+        props: {data},
+    }
 }
--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,5 +1,29 @@
-export default function ContactList() {
+import { render } from "react-dom";
+
+export default function ContactList(data) {
+
+    const contacts = data.data;
+
     return(
-        <h1>Contact List</h1>
+        <div>
+            <h1>Contact List</h1>
+            <ul>
+                {contacts.map((contact, i) => (
+                    <li key={i}>{contact.name}</li>
+                ))}
+            </ul>
+        </div>
     )
+    
+}
+
+
+export async function getServerSideProps() {
+    const url = `https://jsonplaceholder.typicode.com/users/`
+    const req = await fetch(url);
+    const data = await req.json();
+
+    return {
+        props: {data},
+    }
 }
